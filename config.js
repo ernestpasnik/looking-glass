e6d0ac779e315a1895b859b63fe6b95ff72ebee1
Ernest Paśnik <erpa@gmx.com>
2025-09-06 03:34:04 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  company: 'MyCompany',
+  companyUrl: 'https://example.com',
+  themeMode: 'light', // or 'dark'
+  location: 'Data Center 1',
+  locationUrl: 'https://maps.example.com/location1',
+  facility: 'Facility 1',
+  facilityUrl: 'https://peeringdb.com/facility1',
+  ipv4: '192.0.2.1',
+  ipv6: '2001:db8::1',
+  iPerf3incoming: '1 Gbps',
+  iPerf3outgoing: '900 Mbps',
+  stylesheet: 'style.css',
+  speedtestFiles: {
+    'Test File 1': '/files/test1.bin',
+    'Test File 2': '/files/test2.bin'
+  }
+};
